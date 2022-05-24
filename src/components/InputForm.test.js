4e934c15985b491b9181c810d439e9debaeed158
StephenAraka <stevenaraka@gmail.com>
2022-05-24 06:39:10 +0200

    test enable download button

--- src/components/InputForm.test.js
+++ src/components/InputForm.test.js
@@ -5,4 +5,10 @@ test('on initial render, the download button is disabled', async () => {
   render(<InputForm data={[]} />);
 
   expect(await screen.findByRole('button', { name: /download/i })).toBeDisabled();
-})
+});
+
+test('if there are users in the list, the download CSV button becomes enabled', async () => {
+  render(<InputForm data={[{}, {}]} />);
+
+  expect(await screen.findByRole('button', { name: /download/i })).toBeEnabled();
+});
